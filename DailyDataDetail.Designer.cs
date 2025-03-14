c5d16836d91edb343e7665f2dc48206a6fe5b331
Ahsan-Malik-0 <m.ahsan.malik951@gmail.com>
2025-03-14 23:33:41 +0500

    Add Search Functionality

--- DailyDataDetail.Designer.cs
+++ DailyDataDetail.Designer.cs
@@ -42,6 +42,10 @@
             this.label5 = new System.Windows.Forms.Label();
             this.label6 = new System.Windows.Forms.Label();
             this.label7 = new System.Windows.Forms.Label();
+            this.tbSerch = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.rbName = new System.Windows.Forms.RadioButton();
+            this.rbItem = new System.Windows.Forms.RadioButton();
             ((System.ComponentModel.ISupportInitialize)(this.dgvNetWorth)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.dgvSpends)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.dgvRecives)).BeginInit();
@@ -182,11 +186,56 @@
             this.label7.TabIndex = 21;
             this.label7.Text = "Others:";
             // 
+            // tbSerch
+            // 
+            this.tbSerch.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tbSerch.Location = new System.Drawing.Point(281, 12);
+            this.tbSerch.Name = "tbSerch";
+            this.tbSerch.Size = new System.Drawing.Size(137, 22);
+            this.tbSerch.TabIndex = 22;
+            this.tbSerch.TextChanged += new System.EventHandler(this.tbSerch_TextChanged);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(222, 15);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(53, 16);
+            this.label8.TabIndex = 23;
+            this.label8.Text = "Search:";
+            // 
+            // rbName
+            // 
+            this.rbName.AutoSize = true;
+            this.rbName.Location = new System.Drawing.Point(281, 41);
+            this.rbName.Name = "rbName";
+            this.rbName.Size = new System.Drawing.Size(53, 17);
+            this.rbName.TabIndex = 24;
+            this.rbName.TabStop = true;
+            this.rbName.Text = "Name";
+            this.rbName.UseVisualStyleBackColor = true;
+            // 
+            // rbItem
+            // 
+            this.rbItem.AutoSize = true;
+            this.rbItem.Location = new System.Drawing.Point(337, 41);
+            this.rbItem.Name = "rbItem";
+            this.rbItem.Size = new System.Drawing.Size(45, 17);
+            this.rbItem.TabIndex = 25;
+            this.rbItem.TabStop = true;
+            this.rbItem.Text = "Item";
+            this.rbItem.UseVisualStyleBackColor = true;
+            // 
             // DailyDataDetail
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(785, 611);
+            this.Controls.Add(this.rbItem);
+            this.Controls.Add(this.rbName);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.tbSerch);
             this.Controls.Add(this.label7);
             this.Controls.Add(this.label6);
             this.Controls.Add(this.label5);
@@ -230,5 +279,9 @@
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.Label label6;
         private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox tbSerch;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.RadioButton rbName;
+        private System.Windows.Forms.RadioButton rbItem;
     }
 }
--- DailyDataDetail.cs
+++ DailyDataDetail.cs
@@ -15,6 +15,7 @@ namespace Company
     {
         static string conString = @"Data Source=DESKTOP-ND2U4SJ\SQLEXPRESS;Initial Catalog=Company;Integrated Security=True";
         SqlConnection conn = new SqlConnection(conString);
+
         public DailyDataDetail()
         {
             InitializeComponent();
@@ -26,95 +27,164 @@ namespace Company
             dateTimePicker1.CustomFormat = "dd-MM-yyyy";
             dateTimePicker2.Format = DateTimePickerFormat.Custom;
             dateTimePicker2.CustomFormat = "dd-MM-yyyy";
-        }
-
-
-        private void btnSearch_Click(object sender, EventArgs e)
-        {
-            DataSet dataSet = new DataSet();
-
-            //MessageBox.Show("Set the date first");
-            DateTime FromDate = dateTimePicker1.Value;
-            DateTime Todate = dateTimePicker2.Value;
-
-            // Operations On DailyRecords
-            string dailyRecordsQuery = @"SELECT netWorth, differ, FORMAT(date, 'MM-dd-yyyy') AS [Date]
-                                        FROM DailyRecords 
-                                        WHERE date BETWEEN @from AND @to
-                                        ORDER BY date";
-            SqlCommand dailyRecordsCmd = new SqlCommand(dailyRecordsQuery, conn);
-            dailyRecordsCmd.Parameters.AddWithValue("@from", FromDate);
-            dailyRecordsCmd.Parameters.AddWithValue("@to", Todate);
-            SqlDataAdapter sqlDataAdapter1 = new SqlDataAdapter(dailyRecordsCmd);
-
-            // Operations on Spends
-            string spendsQuery = @"SELECT Spends.name AS [Name], Spends.item AS [Item], Spends.cost AS [Cost], Spends.status AS [Status],
-                           FORMAT(DailyRecords.date, 'MM-dd-yyyy') AS [Date]
-                            FROM Spends
-                            JOIN DailyRecords ON DailyRecords.id = Spends.DailyRecordId
-                            WHERE DailyRecords.date BETWEEN @from AND @to
-                            ORDER BY DailyRecords.date;";
-
-            SqlCommand spendsCmd = new SqlCommand(spendsQuery, conn);
-
-            spendsCmd.Parameters.AddWithValue("@from", FromDate);
-            spendsCmd.Parameters.AddWithValue("@to", Todate);
-            SqlDataAdapter sqlDataAdapter2 = new SqlDataAdapter(spendsCmd);
-
-            // Operations on Recives
-            string reciivesQuery = @"SELECT Recives.name AS [Name], Recives.item AS [Item], Recives.cost AS [Cost],
-                            FORMAT(DailyRecords.date, 'MM-dd-yyyy') AS [Date]
-                            FROM Recives
-                            JOIN DailyRecords ON DailyRecords.id = Recives.DailyRecordId
-                            WHERE DailyRecords.date BETWEEN @from AND @to
-                            ORDER BY DailyRecords.date;";
-
-            SqlCommand reciivesCmd = new SqlCommand(reciivesQuery, conn);
-
-            reciivesCmd.Parameters.AddWithValue("@from", FromDate);
-            reciivesCmd.Parameters.AddWithValue("@to", Todate);
-            SqlDataAdapter sqlDataAdapter3 = new SqlDataAdapter(reciivesCmd);
-
-            // Operations on Others
-            string othersQuery = @"SELECT Others.name AS [Name], Others.item AS [Item], Others.cost AS [Cost],
-                            FORMAT(DailyRecords.date, 'MM-dd-yyyy') AS [Date]
-                            FROM Others
-                            JOIN DailyRecords ON DailyRecords.id = Others.DailyRecordId
-                            WHERE DailyRecords.date BETWEEN @from AND @to
-                            ORDER BY DailyRecords.date;";
-
-            SqlCommand othersCmd = new SqlCommand(othersQuery, conn);
-
-            othersCmd.Parameters.AddWithValue("@from", FromDate);
-            othersCmd.Parameters.AddWithValue("@to", Todate);
-            SqlDataAdapter sqlDataAdapter4 = new SqlDataAdapter(othersCmd);
-
-            try
-            {
-                conn.Open();
-
-                sqlDataAdapter1.Fill(dataSet, "DailyRecords");
-                sqlDataAdapter2.Fill(dataSet, "Spends");
-                sqlDataAdapter3.Fill(dataSet, "Recives");
-                sqlDataAdapter4.Fill(dataSet, "Others");
-
-                conn.Close();
-            }
-            catch(Exception ex)
-            {
-                MessageBox.Show(ex.Message);
-            }
-
-
-            dgvNetWorth.DataSource = dataSet.Tables["DailyRecords"];
-            dgvSpends.DataSource = dataSet.Tables["Spends"];
-            dgvRecives.DataSource = dataSet.Tables["Recives"];
-            dgvOthers.DataSource = dataSet.Tables["Others"];
 
             dgvNetWorth.AllowUserToAddRows = false;
             dgvSpends.AllowUserToAddRows = false;
             dgvRecives.AllowUserToAddRows = false;
             dgvOthers.AllowUserToAddRows = false;
+
+            rbName.Checked = true;
+        }
+
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            // Get selected dates from DateTimePickers
+            DateTime fromDate = dateTimePicker1.Value;
+            DateTime toDate = dateTimePicker2.Value;
+
+            // Create a dataset to hold results
+            DataSet dataSet = new DataSet();
+
+            // Define SQL queries
+            string dailyRecordsQuery = @"
+        SELECT netWorth, differ, FORMAT(date, 'MM-dd-yyyy') AS [Date]
+        FROM DailyRecords 
+        WHERE date BETWEEN @from AND @to
+        ORDER BY date";
+
+            string spendsQuery = @"
+        SELECT Spends.name AS [Name], Spends.item AS [Item], Spends.cost AS [Cost], Spends.status AS [Status],
+               FORMAT(DailyRecords.date, 'MM-dd-yyyy') AS [Date]
+        FROM Spends
+        JOIN DailyRecords ON DailyRecords.id = Spends.DailyRecordId
+        WHERE DailyRecords.date BETWEEN @from AND @to
+        ORDER BY DailyRecords.date;";
+
+            string reciivesQuery = @"
+        SELECT Recives.name AS [Name], Recives.item AS [Item], Recives.cost AS [Cost],
+               FORMAT(DailyRecords.date, 'MM-dd-yyyy') AS [Date]
+        FROM Recives
+        JOIN DailyRecords ON DailyRecords.id = Recives.DailyRecordId
+        WHERE DailyRecords.date BETWEEN @from AND @to
+        ORDER BY DailyRecords.date;";
+
+            string othersQuery = @"
+        SELECT Others.name AS [Name], Others.item AS [Item], Others.cost AS [Cost],
+               FORMAT(DailyRecords.date, 'MM-dd-yyyy') AS [Date]
+        FROM Others
+        JOIN DailyRecords ON DailyRecords.id = Others.DailyRecordId
+        WHERE DailyRecords.date BETWEEN @from AND @to
+        ORDER BY DailyRecords.date;";
+
+            try
+            {
+                conn.Open();
+
+                // Load data into dataset
+                LoadTable(dailyRecordsQuery, "DailyRecords", fromDate, toDate, dataSet);
+                LoadTable(spendsQuery, "Spends", fromDate, toDate, dataSet);
+                LoadTable(reciivesQuery, "Recives", fromDate, toDate, dataSet);
+                LoadTable(othersQuery, "Others", fromDate, toDate, dataSet);
+
+                conn.Close();
+
+                // Bind data to DataGridViews
+                dgvNetWorth.DataSource = dataSet.Tables["DailyRecords"];
+                dgvSpends.DataSource = dataSet.Tables["Spends"];
+                dgvRecives.DataSource = dataSet.Tables["Recives"];
+                dgvOthers.DataSource = dataSet.Tables["Others"];
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+        }
+
+        // Function to load data into dataset
+        private void LoadTable(string query, string tableName, DateTime fromDate, DateTime toDate, DataSet dataSet)
+        {
+            using (SqlCommand cmd = new SqlCommand(query, conn))
+            {
+                cmd.Parameters.AddWithValue("@from", fromDate);
+                cmd.Parameters.AddWithValue("@to", toDate);
+
+                using (SqlDataAdapter adapter = new SqlDataAdapter(cmd))
+                {
+                    adapter.Fill(dataSet, tableName);
+                }
+            }
+        }
+
+        private void tbSerch_TextChanged(object sender, EventArgs e)
+        {
+            string word = tbSerch.Text.Trim(); // Get text from the textbox
+            string category = rbName.Checked ? "name" : "item"; // Choose search category
+
+            // Create a proper SQL query with dynamic column name using string interpolation
+            string spendsQuery = $@"
+                                SELECT Spends.name AS [Name], Spends.item AS [Item], Spends.cost AS [Cost], Spends.status AS [Status],
+                                FORMAT(DailyRecords.date, 'MM-dd-yyyy') AS [Date]
+                                FROM Spends
+                                JOIN DailyRecords ON DailyRecords.id = Spends.DailyRecordId
+                                WHERE Spends.{category} LIKE @word
+                                ORDER BY DailyRecords.date;";
+
+            string reciivesQuery = $@"
+                                SELECT Recives.name AS [Name], Recives.item AS [Item], Recives.cost AS [Cost],
+                                FORMAT(DailyRecords.date, 'MM-dd-yyyy') AS [Date]
+                                FROM Recives
+                                JOIN DailyRecords ON DailyRecords.id = Recives.DailyRecordId
+                                WHERE Recives.{category} LIKE @word
+                                ORDER BY DailyRecords.date;";
+
+            string othersQuery = $@"
+                                SELECT Others.name AS [Name], Others.item AS [Item], Others.cost AS [Cost],
+                                FORMAT(DailyRecords.date, 'MM-dd-yyyy') AS [Date]
+                                FROM Others
+                                JOIN DailyRecords ON DailyRecords.id = Others.DailyRecordId
+                                WHERE Others.{category} LIKE @word
+                                ORDER BY DailyRecords.date;";
+
+            try
+            {
+                conn.Open();
+
+                DataSet dataSet = new DataSet(); // Ensure DataSet is initialized
+
+                using (SqlCommand spendsCmd = new SqlCommand(spendsQuery, conn))
+                {
+                    spendsCmd.Parameters.AddWithValue("@word", word + "%");
+                    SqlDataAdapter sqlDataAdapter2 = new SqlDataAdapter(spendsCmd);
+                    sqlDataAdapter2.Fill(dataSet, "searchSpends");
+                }
+
+                using (SqlCommand reciivesCmd = new SqlCommand(reciivesQuery, conn))
+                {
+                    reciivesCmd.Parameters.AddWithValue("@word", word + "%");
+                    SqlDataAdapter sqlDataAdapter3 = new SqlDataAdapter(reciivesCmd);
+                    sqlDataAdapter3.Fill(dataSet, "searchRecives");
+                }
+
+                using (SqlCommand othersCmd = new SqlCommand(othersQuery, conn))
+                {
+                    othersCmd.Parameters.AddWithValue("@word", word + "%");
+                    SqlDataAdapter sqlDataAdapter4 = new SqlDataAdapter(othersCmd);
+                    sqlDataAdapter4.Fill(dataSet, "searchOthers");
+                }
+
+                conn.Close();
+
+                // Bind data to DataGridViews
+                dgvSpends.DataSource = dataSet.Tables["searchSpends"];
+                dgvRecives.DataSource = dataSet.Tables["searchRecives"];
+                dgvOthers.DataSource = dataSet.Tables["searchOthers"];
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
         }
     }
 } 
